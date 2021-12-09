a13437835fe2749760769b1a38835724fb0c92f6
clarice_r <claricerdallorto@gmail.com>
2021-12-09 15:07:32 -0300

    Episode 8 - Make responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="filters flex space-x-6">
-        <div class="w-1/3">
+    <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
+        <div class="w-full md:w-1/3">
             <select name="category" id="category" class="w-full rounded-xl px-4py-2 border-none">
                 <option value="Category One">Category One</option>
                 <option value="Category Two">Category Two</option>
@@ -8,7 +8,7 @@
                 <option value="Category Four">Category Four</option>
             </select>
         </div>
-        <div class="w-1/3">
+        <div class="w-full md:w-1/3">
             <select name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2 border-none">
                 <option value="Filter One">Filter One</option>
                 <option value="Filter Two">Filter Two</option>
@@ -16,7 +16,7 @@
                 <option value="Filter Four">Filter Four</option>
             </select>
         </div>
-        <div class="w-2/3 relative">
+        <div class="w-full md:w-2/3 relative">
             <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="h-4 w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -28,7 +28,7 @@
 
     <div class="ideas-container space-y-6 my-6">
         <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">12</div>
                     <div class="text-gray-500">Votes</div>
@@ -39,21 +39,21 @@
                 </div>
             </div>
 
-            <div class="flex flex-1 px-2 py-6">
-                <div class="flex-none">
+            <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
+                <div class="flex-none mx-4 md:mx-0">
                     <a href="#">
                         <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
                     </a>
                 </div>
-                <div class="w-full mx-4">
-                    <h4 class="text-xl font-semibold">
+                <div class="w-full flex flex-col justify-between mx-4">
+                    <h4 class="text-xl font-semibold mt-2 md:mt-0">
                         <a href="#" class="hover:underline">The title goes here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
                         Lorem ipsum dolor sit amet consectetur.
                     </div>
 
-                    <div class="flex items-center justify-between mt-6">
+                    <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                             <div>10 hours ago</div>
                             <div>&bull;</div>
@@ -63,10 +63,88 @@
                         </div>
                         <div 
                             x-data="{ isOpen: false}"
-                            class="flex items-center space-x-2"
+                            class="flex items-center space-x-2 mt-4 md:mt-0"
                         >
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
-                            
+
+                            <button
+                                @click="isOpen = !isOpen"
+                                class="relative bg-gray-100 border hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in px-3"
+                            >
+                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" style="color: rgba(163, 163, 163, .5)"/>
+                                </svg>
+                                <ul
+                                    x-cloak
+                                    x-show="isOpen"
+                                    x-transition.origin.top.left.duration.300ms
+                                    @click.away="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
+                                >
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+
+                        <div class="flex items-center mt-4 md:mt-0 md:hidden">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none">12</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
+
+                            <button
+                                class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
+                            >
+                            Vote
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end idea-container -->
+
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl text-blue">66</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-blue hover:bg-blue-hover text-white font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Voted</button>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
+                <div class="flex-none mx-2 md:mx-0">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                    </a>
+                </div>
+                <div class="w-full flex flex-col justify-between mx-2 md:mx-4">
+                    <h4 class="text-xl font-semibold mt-2 md:mt-0">
+                        <a href="#" class="hover:underline">The title goes here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </div>
+
+                    <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div 
+                            x-data="{ isOpen: false}"
+                            class="flex items-center space-x-2 mt-4 md:mt-0"
+                        >
+                            <div class="bg-yellow text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>
+
                             <button
                                 @click="isOpen = !isOpen"
                                 class="relative bg-gray-100 border hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in px-3"
@@ -87,49 +165,17 @@
                                 </ul>
                             </button>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end idea-container -->
 
-        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
-                <div class="text-center">
-                    <div class="font-semibold text-2xl text-blue">66</div>
-                    <div class="text-gray-500">Votes</div>
-                </div>
+                        <div class="flex items-center mt-4 md:mt-0 md:hidden">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none text-blue">66</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
 
-                <div class="mt-8">
-                    <button class="w-20 bg-blue hover:bg-blue-hover text-white font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Voted</button>
-                </div>
-            </div>
-
-            <div class="flex px-2 py-6">
-                <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
-                </a>
-                <div class="mx-4">
-                    <h4 class="text-xl font-semibold">
-                        <a href="#" class="hover:underline">Another title goes here</a>
-                    </h4>
-                    <div class="text-gray-600 mt-3 line-clamp-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quo optio, consequatur nihil minus obcaecati ex! Obcaecati temporibus nobis minima incidunt reprehenderit corrupti amet architecto! Earum ut quasi minus facere sapiente aperiam cupiditate quis commodi error, saepe, quam, doloribus nisi eligendi perferendis aspernatur dolores! Ad assumenda laudantium laboriosam. Suscipit eaque magni eligendi tenetur nemo, amet vero voluptatibus quidem laudantium error. Reprehenderit consequuntur cum quas vero est sunt labore officia harum tenetur quia quidem, debitis reiciendis unde esse exercitationem nulla incidunt illo, quisquam ducimus, sit illum accusamus eius? Eligendi tempora iure, veritatis tenetur, nobis quibusdam et ut fugit, necessitatibus quis magnam.
-                    </div>
-
-                    <div class="flex items-center justify-between mt-6">
-                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                            <div>10 hours ago</div>
-                            <div>&bull;</div>
-                            <div>Category 1</div>
-                            <div>&bull;</div>
-                            <div class="text-gray-900">3 Comments</div>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <div class="bg-yellow text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>
-                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in px-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" style="color: rgba(163, 163, 163, .5)"/>
-                                </svg>
+                            <button
+                                class="w-20 bg-blue hover:bg-blue-hover text-white font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
+                            >
+                            Voted
                             </button>
                         </div>
                     </div>
@@ -137,7 +183,7 @@
             </div>
         </div> <!-- end idea-container -->
 
-        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+        {{-- <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
             <div class="border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">32</div>
@@ -270,7 +316,8 @@
                     </div>
                 </div>
             </div>
-        </div> <!-- end idea-container -->
+        </div> --}}
+        <!-- end idea-container -->
         
     </div> <!-- end ideas-container -->
 </x-app-layout>
